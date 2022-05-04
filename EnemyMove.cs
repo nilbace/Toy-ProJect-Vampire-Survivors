530512b1905d726103d23afae41251786104f8df
nilbace <101455214+nilbace@users.noreply.github.com>
2022-05-04 18:23:53 +0900

    Update EnemyMove.cs

--- EnemyMove.cs
+++ EnemyMove.cs
@@ -1,3 +1,5 @@
+//적의 이동 구현
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +9,7 @@ public class EnemyMove : MonoBehaviour
 
     Rigidbody2D enemyRigidbody2d;
     [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float magnitude = 0.3f;
+    [SerializeField] float magnitude = 0.3f; //떨리는 정도
     [SerializeField] float waittime = 0.2f;
     [Header("Jewel")]
     [SerializeField] GameObject jewel;
@@ -20,18 +22,18 @@ public class EnemyMove : MonoBehaviour
         enemyRigidbody2d = GetComponent<Rigidbody2D>();
         damageHealth = GetComponent<DamageHealth>();
         Player = GameObject.Find("Player");
-        StartCoroutine(ShiverMove());
+        StartCoroutine(ShiverMove());  //떨리는 움직임을 구현함
     }
 
     // Update is called once per frame
     void Update()
     {
-        FollowPlayer();
+        FollowPlayer();  //계속 플레이어를 쫓아옴
         int health = damageHealth.GetHealth();
 
         if(health <= 0 && once)
         {
-            Instantiate(jewel, transform.position, Quaternion.identity);
+            Instantiate(jewel, transform.position, Quaternion.identity);//죽으면 보석 반환
             once = false;
         }
     }
@@ -40,7 +42,7 @@ public class EnemyMove : MonoBehaviour
     {
         float delta = moveSpeed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, delta);
-        if(Player.transform.position.x >= transform.position.x)
+        if(Player.transform.position.x >= transform.position.x) //플레이어를 바라봄
             transform.localScale = new Vector2 (-1, 1);
         else
             transform.localScale = new Vector2(1, 1);
@@ -50,7 +52,7 @@ public class EnemyMove : MonoBehaviour
     {
         while(true)
         {
-            transform.position += (Vector3)Random.insideUnitCircle * magnitude;
+            transform.position += (Vector3)Random.insideUnitCircle * magnitude;  //조금씩 랜덤하게 떨
             yield return new WaitForSeconds(waittime);
         }
     }
