17723ebd4dc706c228434a3798f513744443b37d
nilbace <101455214+nilbace@users.noreply.github.com>
2022-05-04 18:21:15 +0900

    Update DamageHealth.cs

--- DamageHealth.cs
+++ DamageHealth.cs
@@ -1,11 +1,13 @@
+//플레이어, 무기, 적 모두에게 붙어있는 스크립트
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageHealth : MonoBehaviour
 {
-    [SerializeField] Color32 red;
-    [SerializeField] Color32 original;
+    [SerializeField] Color32 red;  //빨간색
+    [SerializeField] Color32 original;  //원래 색
     [SerializeField] public int health = 50;
     [SerializeField] int Damage = 25;
     [SerializeField] bool isWeapon = false;
@@ -20,18 +22,18 @@ public class DamageHealth : MonoBehaviour
     void Update()
     {
         if(!isWeapon && health < 0)
-        Destroy(gameObject);
+        Destroy(gameObject); 
     }
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(!isWeapon)
+        if(!isWeapon)  //피격당할시(무기가 아니라면)
         {
             int collisonDamage = other.GetComponent<DamageHealth>().GetDamage();
             RedColor();
-            health -= collisonDamage;
+            health -= collisonDamage; 
         }
-        else
+        else   //무기는 적과 닿을시 사라짐
             Destroy(gameObject);
     }
 
