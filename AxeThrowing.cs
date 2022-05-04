0969da262e76dd3c5987498b360195a62a4b8406
nilbace <101455214+nilbace@users.noreply.github.com>
2022-05-04 18:17:08 +0900

    Update AxeThrowing.cs
    도끼Prefab에 붙어있는 스크립트

--- AxeThrowing.cs
+++ AxeThrowing.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AxeThrowing : MonoBehaviour
 {
     Rigidbody2D AxeRigidbody;
-    GameObject Player;
+    GameObject Player;//플레이어가 바라보는 방향을 참조하기 위함
 
     void Start()
     {
@@ -16,7 +16,7 @@ public class AxeThrowing : MonoBehaviour
 
     void Update()
     {
-        if(Time.timeScale == 0f)
+        if(Time.timeScale == 0f)//일시정지상태일때 도끼의 회전을 멈춤
             return;
         else
             transform.Rotate(0, 0, 2f);
@@ -24,7 +24,7 @@ public class AxeThrowing : MonoBehaviour
 
     void HasThrown()
     {
-        float LeftRight = Player.transform.localScale.x;
+        float LeftRight = Player.transform.localScale.x;//플레이어가 어느방향을 바라보는지 확인
         AxeRigidbody.velocity = new Vector2(LeftRight * 5f, 3f);
     }
 
